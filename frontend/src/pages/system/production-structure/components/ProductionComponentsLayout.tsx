ba697dddbb108510b07139031a10762d8be54680
mihai <mihaipop@outlook.com>
2026-05-29 11:57:44 -0700

    0529 1157

--- frontend/src/pages/system/production-structure/components/ProductionComponentsLayout.tsx
+++ frontend/src/pages/system/production-structure/components/ProductionComponentsLayout.tsx
@@ -233,7 +233,7 @@ function ProductionComponentsShell() {
           <div className="shrink-0 bg-muted" style={{ width: 1 }} />
           <div className="flex flex-col overflow-hidden flex-1 min-w-0">
             <Toolbar />
-            <div className="flex min-h-0 flex-1 overflow-hidden">
+            <div className="flex min-h-0 flex-1 flex-col overflow-hidden">
               <Outlet />
             </div>
           </div>
